2527276989d519e5e887bc96884bb6aaea79a3c0
Catalin Climov <climov@gmail.com>
2023-05-15 12:42:19 +0300

    Fixed Norwegian dictionaries to use UTF-8 encoding instead of ISO8859-1

--- scripts/iconv.sh
+++ scripts/iconv.sh
@@ -3,22 +3,22 @@ for i in `cat encodings.csv | cut -d',' -f 1`
 do
     senc=`cat encodings.csv | grep "${i}" | cut -d',' -f 2`
     #echo "${i} ${senc}"
-    if [ ! -f "../_hunspell/${i}/${i}.aff" ]
+    if [ ! -f "../dictionaries/${i}/${i}.aff" ]
     then
         echo "Not found"
         exit 1
     fi
-    if [ ! -f "../_hunspell/${i}/${i}.dic" ]
+    if [ ! -f "../dictionaries/${i}/${i}.dic" ]
     then
         echo "Not found"
         exit 1
     fi
-    #cp "../_hunspell/${i}/${i}.aff" "../_hunspell/${i}/${i}.aff.orig"
-    #cp "../_hunspell/${i}/${i}.dic" "../_hunspell/${i}/${i}.dic.orig"
+    cp "../dictionaries/${i}/${i}.aff" "../dictionaries/${i}/${i}.aff.orig"
+    cp "../dictionaries/${i}/${i}.dic" "../dictionaries/${i}/${i}.dic.orig"
     #rm -f "../_hunspell/${i}/${i}.aff.new"
     #rm -f "../_hunspell/${i}/${i}.dic.new"
-    echo "iconv -f ${senc} -t UTF-8 \"../_hunspell/${i}/${i}.aff.orig\" >\"../_hunspell/${i}/${i}.aff\" || exit 1"
-    iconv -f ${senc} -t UTF-8 "../_hunspell/${i}/${i}.aff.orig" >"../_hunspell/${i}/${i}.aff" || exit 1
-    echo "iconv -f ${senc} -t UTF-8 \"../_hunspell/${i}/${i}.dic.orig\" >\"../_hunspell/${i}/${i}.dic\" || exit 1"
-    iconv -f ${senc} -t UTF-8 "../_hunspell/${i}/${i}.dic.orig" >"../_hunspell/${i}/${i}.dic" || exit 1
+    echo "iconv -f ${senc} -t UTF-8 \"../dictionaries/${i}/${i}.aff.orig\" >\"../dictionaries/${i}/${i}.aff\" || exit 1"
+    iconv -f ${senc} -t UTF-8 "../dictionaries/${i}/${i}.aff.orig" >"../dictionaries/${i}/${i}.aff" || exit 1
+    echo "iconv -f ${senc} -t UTF-8 \"../dictionaries/${i}/${i}.dic.orig\" >\"../dictionaries/${i}/${i}.dic\" || exit 1"
+    iconv -f ${senc} -t UTF-8 "../dictionaries/${i}/${i}.dic.orig" >"../dictionaries/${i}/${i}.dic" || exit 1
 done
